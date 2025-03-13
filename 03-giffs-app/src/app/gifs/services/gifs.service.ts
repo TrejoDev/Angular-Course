31c79906d2c129b9703fb5f7c21c864dcb86bfc1
Diam <trejodev@gmail.com>
2025-03-13 13:07:41 +0100

    GifsApp done

--- 03-giffs-app/src/app/gifs/services/gifs.service.ts
+++ 03-giffs-app/src/app/gifs/services/gifs.service.ts
@@ -22,7 +22,17 @@ export class GifsService {
   private http = inject(HttpClient);
 
   tredingGifs = signal<Gif[]>([]);
-  tredingGifsLoadind = signal<boolean>(true);
+  tredingGifsLoadind = signal<boolean>(false);
+  private trendingPage = signal(0);
+
+  trendingGifGroup = computed<Gif[][]>(() => {
+    const groups = [];
+    for (let i = 0; i < this.tredingGifs().length; i += 3) {
+      groups.push(this.tredingGifs().slice(i, i + 3));
+    }
+
+    return groups; //[[gif,gif,gif,],[gif,gif,gif,],[gif,gif,gif,]] Para mi diseño Masonry
+  });
 
   searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
   searchHistoryKey = computed(() => Object.keys(this.searchHistory()));
@@ -37,16 +47,22 @@ export class GifsService {
   });
 
   loadTrendingGifs() {
+    if (this.tredingGifsLoadind()) return;
+
+    this.tredingGifsLoadind.set(true);
+
     this.http
       .get<GiphyResponse>(`${environment.gihpyUrl}/gifs/trending`, {
         params: {
           api_key: environment.gihpyApiKey,
           limit: 20,
+          offset: this.trendingPage() * 20,
         },
       })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifsArray(resp.data);
-        this.tredingGifs.set(gifs);
+        this.tredingGifs.update((currentGifs) => [...currentGifs, ...gifs]);
+        this.trendingPage.update((v) => (v += 1));
         this.tredingGifsLoadind.set(false);
       });
   }
